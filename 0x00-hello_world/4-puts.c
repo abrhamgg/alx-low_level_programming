0421cda48fbe2f0875a17eb46721054d80d26c62
abrhamgg <abrhamgg@gmail.com>
2022-03-10 10:16:47 -0800

    first c program with put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main ()
+/**
+ * function main returns 0 if the code runs without any error
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

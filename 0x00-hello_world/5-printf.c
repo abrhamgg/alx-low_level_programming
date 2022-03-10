86b697c4799d9918d37cbaaad0ce2ce10f270092
abrhamgg <abrhamgg@gmail.com>
2022-03-10 10:27:53 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * return: 0 if the code runs without error
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

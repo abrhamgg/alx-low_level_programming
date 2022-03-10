6aafd096dc83791aca72cc4c0c541ec2e83551fe
abrhamgg <64625512+abrhamgg@users.noreply.github.com>
2022-03-10 22:21:39 +0300

    trailing white space

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,19 +6,18 @@
  * Return: Always 0 (success)
  */
 
-int main(void)
-{
-	int inttype;
-	float floattype;
-	long int longint;
-	long long int longlong;
-	char chartype;
+int main(void) {
+        int inttype;
+        float floattype;
+        long int longint;
+        long long int longlong;
+        char chartype;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(chartype));
-	printf("Size of an int: %zu byte(s)\n", sizeof(inttype));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(longint));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlong));
-	printf("Size of a float: %zu byte(s)\n", sizeof(floattype));
-	
-	return (0);
+        printf("Size of a char: %zu byte(s)\n", sizeof(chartype));
+        printf("Size of an int: %zu byte(s)\n", sizeof(inttype));
+        printf("Size of a long int: %zu byte(s)\n", sizeof(longint));
+        printf("Size of a long long int: %zu byte(s)\n", sizeof(longlong));
+        printf("Size of a float: %zu byte(s)\n", sizeof(floattype));
+
+        return (0);
 }

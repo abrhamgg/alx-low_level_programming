3fb1a83d8e39f7aa5de2652c0f8dd7a4c7871ad6
abrhamgg <abrhamgg@gmail.com>
2022-03-15 20:23:55 +0300

    function and nested loop

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,17 +4,3 @@
 int _putchar(char c);
 
 #endif
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: the character to print
- * 
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropirately
- */
-
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}

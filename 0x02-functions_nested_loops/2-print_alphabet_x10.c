09c2f0d7469bbd3441f095cadfc84df1357a2594
abrhamgg <abrhamgg@gmail.com>
2022-03-15 21:23:37 +0300

    function and nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10: prints alphabets in lowercase 10 times.
+ * print_alphabet_x10 -  prints alphabets in lowercase 10 times.
  *
  * Return: Always 0
  */
@@ -19,5 +19,7 @@ void print_alphabet_x10(void)
 			_putchar(j);
 		}
 		_putchar('\n');
+		i++;
 	}
+	return (0);
 }

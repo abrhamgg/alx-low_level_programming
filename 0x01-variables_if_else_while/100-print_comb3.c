0e85d324128605278899889515cb04fa643ca78d
abrhamgg <abrhamgg@gmail.com>
2022-03-15 20:12:11 +0300

    function and nested loop

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			printf("%d%d, ", i, j);
+		}
+	}
+	return (0);
+}

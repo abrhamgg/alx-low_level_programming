e6b45c670b5d3e5e1fbca24f7ed2a8b000fa9469
abrhamgg <abrhamgg@gmail.com>
2022-03-15 20:37:05 +0300

    function and nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-    char ch[] = "_putchar";
-    int i;
+	char ch[] = "_putchar";
+	int i;
 
-    for (i = 0; i < 8; i++)
-    {
-	    _putchar(ch[i]);
-    }
-    _putchar('\n');
-    return (0);
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(ch[i]);
+	}
+	_putchar('\n');
+	return (0);
 }

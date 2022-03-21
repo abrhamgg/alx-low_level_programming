d93e9649c70e13182bcb51eb04958838ce74d353
abrhamgg <64625512+abrhamgg@users.noreply.github.com>
2022-03-21 20:24:29 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,18 +14,16 @@ void rev_string(char *s)
     int i;
     int tmp;
     int len = 0;
-    int x, n;
+    int x;
 
     for (i = 0; *(s + i) != '\0'; i++)
     {
         len++;
     }
     len = len -1;
-    //printf("%d\n", len);
     for (i = 0; i < len/2; i++)
     {
         x = len - 1;
-       // printf("%d", x);
         tmp = s[i];
         s[i] = s[x];
         s[x] = tmp;

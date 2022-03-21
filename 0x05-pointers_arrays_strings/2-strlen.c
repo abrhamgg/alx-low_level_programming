a8de23776a2334b6b888104a20e6ce3faca022f6
abrhamgg <abrhamgg@gmail.com>
2022-03-21 12:38:20 +0300

    more function and nested loop

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that counts a string and returns
+ * the number of characters.
+ *
+ * @s: input to the above function.
+ *
+ * Return: returns an int which is the number of
+ * characters.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0; *(s + i) != NULL; i++)
+	{
+		len++;
+	}
+	return
+		putchar(len);
+}

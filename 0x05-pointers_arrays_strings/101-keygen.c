2f6bc1a89dbe31cbe4093a8fe92432f05dfeba66
abrhamgg <abrhamgg@gmail.com>
2022-03-21 23:19:02 +0300

    pointer and arrays

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,60 +1,51 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - program that generates random passwords
+ * for program crackme.
+ *
+ * Return: Always 0.
+ */
 
-
-int main()
+int main(void)
 {
-	int N = rand() % 8;
-  int i = 0;
+	int i;
+	int N = rand() % 25;
+	char *Letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *Number = "123456789";
+	char *symbol = "!@#$^&*?";
+	char *letter = "abcdefghijklmnopqrstuvwxyz";
+	int randomizer = rand() % 4;
+	char password[50];
 
-	int randomizer = 0;
-
-	srand((unsigned int)(time(NULL)));
-
-	char *numbers[] = "0123456789";
-
-	
-	char *letter[] = "abcdefghijklmnoqprstuvwyzx";
-
-	char *LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-
-	
-	char *symbols[] = "!@#$^&*?";
-
-	char *password[N];
-
-	randomizer = rand() % 4;
-
-	for (i = 0; i < N; i++) {
-
-		if (randomizer == 1) {
-			password[i] = numbers[rand() % 10];
+	for (i = 0; i < N; i++)
+	{
+		if (randomizer == 1) 
+		{
+			password[i] = Number[rand() % 10];
 			randomizer = rand() % 4;
 			printf("%c", password[i]);
 		}
-		else if (randomizer == 2) {
-			password[i] = symbols[rand() % 8];
+		else if (randomizer == 2) 
+		{
+			password[i] = symbol[rand() % 8];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);	
+		}
+		else if (randomizer == 3) 
+		{
+			password[i] = Letter[rand() % 26];
 			randomizer = rand() % 4;
 			printf("%c", password[i]);
 		}
-		else if (randomizer == 3) {
-			password[i] = LETTER[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else {
+		else 
+		{
 			password[i] = letter[rand() % 26];
 			randomizer = rand() % 4;
 			printf("%c", password[i]);
 		}
+		return 0
 	}
-  
-
-	// Function Call
-	//randomPasswordGeneration(N);
-
-	return 0;
 }

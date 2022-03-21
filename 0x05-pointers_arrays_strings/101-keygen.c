e1244a73a96870c5ee475cbec96eca6c2839e1cb
abrhamgg <64625512+abrhamgg@users.noreply.github.com>
2022-03-21 22:51:13 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,38 +8,27 @@
 // Driver Code
 int main()
 {
-	// Length of the password to
-	// be generated
 	int N = rand() % 8;
   int i = 0;
 
 	int randomizer = 0;
 
-	// Seed the random-number generator
-	// with current time so that the
-	// numbers will be different every time
 	srand((unsigned int)(time(NULL)));
 
-	// Array of numbers
 	char numbers[] = "0123456789";
 
-	// Array of small alphabets
+	
 	char letter[] = "abcdefghijklmnoqprstuvwyzx";
 
-	// Array of capital alphabets
 	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
 
-	// Array of all the special symbols
+	
 	char symbols[] = "!@#$^&*?";
 
-	// Stores the random password
 	char password[N];
 
-	// To select the randomizer
-	// inside the loop
 	randomizer = rand() % 4;
 
-	// Iterate over the range [0, N]
 	for (i = 0; i < N; i++) {
 
 		if (randomizer == 1) {

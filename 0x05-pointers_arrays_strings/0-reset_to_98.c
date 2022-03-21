2c657f0197750a96b93d8504481a7ee10d9eee75
abrhamgg <64625512+abrhamgg@users.noreply.github.com>
2022-03-21 10:56:00 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * reset_to_98 - function that takes pointer
- as an input and modify its value to 98.
+ * reset_to_98 - function that takes pointer as
+ * an input and modify its value to 98.
  *
  * @n: input to the function.
  */

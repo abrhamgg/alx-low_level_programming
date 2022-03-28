edaacddeb276d145d1433bce34d01cfb758d2663
abrhamgg <64625512+abrhamgg@users.noreply.github.com>
2022-03-28 22:19:00 +0300

    pointer and arrays

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,26 +1,24 @@
 #include "main.h"
 
 /**
- * _strpbrk - bytes
- * @s: pointer to char
- * @accept: pointer to char
- * Return: NULL
+ * _strpbrk - searching a string for any of a set of bytes
+ * @s: the string
+ * @accept: set of bytes
+ *
+ * Return: a pointer to the byte in s that matches one of the bytes in accept
  */
-
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
+	unsigned int i;
+	unsigned int j;
 
-	while (*s)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; accept[i]; i++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*s == accept[i])
-			{
-				return (s);
-			}
+			if (s[i] == accept[j])
+				return (&s[i]);
 		}
-		s++;
 	}
 	return (NULL);
 }

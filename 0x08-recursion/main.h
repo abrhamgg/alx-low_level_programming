ff79e896e5723164ab4df76e0d0ad5d2a1c41bb4
abrhamgg <abrhamgg@gmail.com>
2022-03-30 20:38:19 +0300

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,8 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int _sqrt_recursion(int n);
 int _sqrt(int n, int i);
+int is_prime_number(int n);
+int divisors(int n, int m);
+int tmp_prime(int n, int i);
 
 #endif

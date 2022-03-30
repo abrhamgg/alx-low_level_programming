98ba2e1ebf283a6beb347d56029914df55d0818c
abrhamgg <abrhamgg@gmail.com>
2022-03-30 16:30:30 +0300

    pointer and array

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,7 +4,7 @@
  * _strlen_recursion - function that prints number
  * of chars in string.
  * @s: input string.
- * return: length of the string.
+ * Return: length of the string.
  */
 
 int _strlen_recursion(char *s)

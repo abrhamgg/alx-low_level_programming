44a363f39f1dbae20b65f36ba4e4b0ba1b1769e1
abrhamgg <abrhamgg@gmail.com>
2022-04-01 22:55:08 +0300

    command line argument

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - function that prints minimum number of
+ * coins to make change for amount.
+ * @argc: no of argument.
+ * @argv: array of arguments.
+ * Return: 0 (Success);
+ */
+
+int main(int argc, char *argv[])
+{
+	int c = atoi(argv[1]);
+	int count = 0;
+}

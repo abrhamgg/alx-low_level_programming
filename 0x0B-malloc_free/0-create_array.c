55a8a36267513ebe6ee30a0e7e3a6cb10d06a1ac
abrhamgg <abrhamgg@gmail.com>
2022-04-05 19:57:34 +0300

    dynamic memory allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,15 @@
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char *p;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *p = malloc(size);
-	for (i == 0; i < size; i++)
+	p = malloc(size);
+	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
 	}

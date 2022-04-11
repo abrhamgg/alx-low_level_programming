e25d4531cd73a05ad3d25f02f8b2ae439ba6328b
abrhamgg <abrhamgg@gmail.com>
2022-04-11 11:50:05 +0300

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER
+#define HEADER
+
+/**
+ * struct dog - structure of a dog.
+ * @name: member 1
+ * @age: member 2
+ * @owner: member 3.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

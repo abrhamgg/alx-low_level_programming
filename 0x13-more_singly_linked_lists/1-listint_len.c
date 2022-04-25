e745ae9b7f5092fdb2847a1d4f19f4e058857838
abrhamgg <abrhamgg@gmail.com>
2022-04-25 12:28:34 +0300

    more on linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - function that returns no of elements in a linked list.
+ * @h: linked list.
+ * Return: number of nodes.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+        unsigned int c = 0;
+
+        while (h != NULL)
+        {
+                h = h->next;
+                c++;
+        }
+        return (c);
+}
+

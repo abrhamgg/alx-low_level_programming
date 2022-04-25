0436f76ba63736fac0a44a307a421eaea05d7d92
abrhamgg <abrhamgg@gmail.com>
2022-04-25 21:37:34 +0300

    fixed betty

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,13 +9,13 @@
 
 size_t listint_len(const listint_t *h)
 {
-        unsigned int c = 0;
+	unsigned int c = 0;
 
-        while (h != NULL)
-        {
-                h = h->next;
-                c++;
-        }
-        return (c);
+	while (h != NULL)
+	{
+		h = h->next;
+		c++;
+	}
+	return (c);
 }
 

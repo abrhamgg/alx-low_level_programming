6b27db5095d4f2a93c7081a7b1ce79514a9e65df
abrhamgg <abrhamgg@gmail.com>
2022-06-09 18:05:48 +0300

    free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *free_dlistint- frees the memory aquired by ptr
+ *@head: ptr to doubly linked list
+ *
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+	head = NULL;
+
+}

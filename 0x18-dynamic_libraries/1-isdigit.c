d703dc4e9a0e1c4a1cba72afb292d29fb6d8bc61
abrhamgg <abrhamgg@gmail.com>
2022-06-13 15:21:07 +0300

    creating dynamic library

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code.
+ * @c: Variable text
+ * Return: check the letter and return 1 if its digit, 0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	return (1);
+	return (0);
+}
+

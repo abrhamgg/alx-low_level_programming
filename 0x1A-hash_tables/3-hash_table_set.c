525b46928fa46e903410296d37001a0815603210
abrhamgg <abrhamgg@gmail.com>
2022-07-01 16:52:11 +0300

    hash_set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,6 +12,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *new = malloc(sizeof(hash_node_t));
 
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
 	if (new == NULL)
 		return (0);
 	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)

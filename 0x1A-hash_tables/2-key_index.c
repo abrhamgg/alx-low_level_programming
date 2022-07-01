deb3d7a707a5ddde1d54769e29ce32a11c0fbb21
abrhamgg <abrhamgg@gmail.com>
2022-07-01 14:06:25 +0300

    betty

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,8 +1,9 @@
 #include "hash_tables.h"
 /**
- * key-index function tht gives you index of a key
+ * key_index - function tht gives you index of a key
  * @key: is the key
- * @size - size of the array of hash table
+ * @size: size of the array of hash table
+ * Return: index of a key within the array.
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)

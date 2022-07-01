5436fe4ad4a6dbe166d797a224613ca42be58d2d
abrhamgg <abrhamgg@gmail.com>
2022-07-01 14:58:10 +0300

    init hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	unsigned long int i;
 
 	if (size == 0)
 	return (NULL);
@@ -22,5 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(ht);
 		return (NULL);
 	}
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 	return (ht);
 }
